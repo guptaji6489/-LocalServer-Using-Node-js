5aa8846cc24bcfdf61783a60e899712ed92584fc
Harshit Gupta <46419188+guptaji6489@users.noreply.github.com>
2019-09-19 20:11:35 +0530

    Update myserver.js

--- myserver.js
+++ myserver.js
@@ -2,8 +2,8 @@ var http = require('http');
 
 http.createServer(function(request, response){
         response.writeHead(500,{'Content-Type':'text/html'});
-        response.end('<h1>Welcome to Hello World Localserver using Node.js</h1>');
+        response.end('<h1>This is Localserver using Node.js</h1>');
 
-}).listen(5244);
-console.log('running server on port no 5244');
+}).listen(3000);
+console.log('running server on port no 3000');
 
